30a9277743d76bfebdcb19f5707bfa8e2bd49b0a
gdayramirezdev <josegustavo.ramirez.dev@outlook.com>
2023-12-22 00:40:50 -0600

    agregando los filtros, primera version

--- src/controllers/BlogController.ts
+++ src/controllers/BlogController.ts
@@ -1,13 +1,61 @@
 import { Request, Response } from 'express';
 import BlogService from '../services/BlogService';
+import { TBlog } from '../types';
+import { Op } from '@sequelize/core';
 
 export class BlogController {
-  static getAllBlogs(_: Request, response: Response): void {
-    BlogService.getAll()
+  static getAllBlogs(request: Request, response: Response): void {
+    const { title, autor, content } = request.query;
+    let params = {
+      where: {},
+    };
+
+    if (title) {
+      params.where = {
+        ...params.where,
+        title: {
+          [Op.like]: `%${title}%`
+        }
+      }
+    }
+
+    if (autor) {
+      params.where = {
+        ...params.where,
+        autor: {
+          [Op.like]: `%${autor}%`
+        }
+      }
+    }
+
+    if (content) {
+      params.where = {
+        ...params.where,
+        content: {
+          [Op.like]: `%${content}%`
+        }
+      }
+    }
+
+    BlogService.getAll(params)
       .then((result) => response.status(200).send({ message: 'success', data: result }))
       .catch((error) => response.status(400).send({ message: 'error', data: error }));
   }
 
+  static getOneBlog(request: Request, response: Response): void {
+    const { id } = request.params;
+    BlogService.getOne(parseInt(id))
+      .then((result) => response.status(200).send({ message: 'success', data: result }))
+      .catch((error) => response.status(400).send({ message: 'error', data: error }));
+  }
+
+  static createBlog(request: Request, response: Response): void{
+    const { body } = request;
+    console.log('Body', body)
+    BlogService.createOne(body)
+      .then((result) => response.status(200).send({ message: 'success', data: result }))
+      .catch((error) => response.status(400).send({ message: 'error', data: error }));
+  }
 
 }
 
